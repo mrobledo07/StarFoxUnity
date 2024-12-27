5d3b795b1cc6a825144b097027cf23e564797dc9
Miguel Robledo <mrobledo027@gmail.com>
2024-12-27 21:07:04 +0100

    arwing (no ben rotat)

--- Assets/Scripts/Arwing/ArwingMovement.cs
+++ Assets/Scripts/Arwing/ArwingMovement.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 
-public class ArwingMovement : MonoBehaviour
+public class ShipMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float verticalSpeed = 20f;
+    public float horizontalSpeed = 40f;
 
-    // Update is called once per frame
+    // Update se llama una vez por frame
     void Update()
     {
-        
+        // Mover la nave hacia arriba o abajo con las teclas W y S o las flechas
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+            float moveVertical = -Input.GetAxis("Vertical");
+            transform.Translate(Vector3.up * moveVertical * verticalSpeed * Time.deltaTime);
+        }
+        // Rotar la nave con las teclas A y D o las flechas
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * moveHorizontal * horizontalSpeed * Time.deltaTime);
+        }
+      
     }
 }
